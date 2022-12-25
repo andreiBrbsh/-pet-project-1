f336fa63bfb1d01e8f9fe0c188c0521421ee597c
andreiBrbsh <andrei.brbsh@icloud.com>
2022-12-25 19:21:56 +0200

    header + slider

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+
+
+// SLIDER --------
+const swiper = new Swiper('.body-main-block__slider', {
+      loop: true,
+      navigation: {
+        nextEl: '.body-main-block__arrow.swiper-button-next',
+        prevEl: '.body-main-block__arrow.swiper-button-prev',
+      },
+});
+
