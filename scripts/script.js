d19ae59cb3f43d5340d333b50ede218da7921bde
andreiBrbsh <andrei.brbsh@icloud.com>
2023-01-02 15:36:07 +0200

    tabs

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,58 @@ const swiper = new Swiper('.body-main-block__slider', {
 
 // TABS ----------
 
+// buttons
+const tabNavItems = document.querySelectorAll('.tabs-deals__button');
+
+// tabs
+const tabItems = document.querySelectorAll('.item-tabs');
+
+
+
+document.addEventListener('click', function(e) {
+  const targetElement = e.target;
+  let currentActiveIndex = null;
+  let newActiveIndex = null;
+                      
+  if (targetElement.closest('.tabs-deals__button')) {
+    tabNavItems.forEach((tabNavItem, index) => {
+      if (tabNavItem.classList.contains('active')) {
+        currentActiveIndex = index;
+        tabNavItem.classList.remove('active');
+      } 
+      if (tabNavItem === targetElement) {
+        newActiveIndex = index;
+      }
+    });
+    targetElement.classList.add('active');
+    tabItems[currentActiveIndex].classList.remove('active');
+    tabItems[newActiveIndex].classList.add('active');
+  } 
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
